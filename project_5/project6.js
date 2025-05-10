d3ae47ca2e918a6a4e14d469cbea2939f302bbfb
MikeTango2002 <miketango2002@gmail.com>
2025-05-10 17:27:59 +0200

    Project 5 finished

--- project_5/project6.js
+++ project_5/project6.js
@@ -33,25 +33,48 @@ uniform Light  lights [ NUM_LIGHTS  ];
 uniform samplerCube envMap;
 uniform int bounceLimit;
 
+const float BIAS_PRIMARY = 0.005; // To avoid self-intersection
+
 bool IntersectRay( inout HitInfo hit, Ray ray );
 
 // Shades the given point and returns the computed color.
 vec3 Shade( Material mtl, vec3 position, vec3 normal, vec3 view )
 {
+	// variables intitialization
 	vec3 color = vec3(0,0,0);
+	Ray shadowRay;
+	vec3 v = normalize(-view);
+	vec3 n = normalize(normal);
+	vec3 omega;
+	vec3 h;
+	float cos_theta;
+	float cos_fi;
+
+
 	for ( int i=0; i<NUM_LIGHTS; ++i ) {
 		// TO-DO: Check for shadows
+		shadowRay.dir = normalize(lights[i].position - position);
+		shadowRay.pos = position + shadowRay.dir * 0.05;
+		HitInfo shadowHit;
+		bool isShadowed = false;
+
+		if (IntersectRay(shadowHit, shadowRay)) {
+			if (shadowHit.t > 0.0) // True intersection with another object
+				isShadowed = true;
+		}
 		// TO-DO: If not shadowed, perform shading using the Blinn model
 		// normalize vectors
-		vec3 v = normalize(-view);
-		vec3 omega = normalize(lights[i].position);
-		vec3 n = normalize(normal);
-		vec3 h = normalize(omega + v);
-		//
-		float cos_theta = max(dot(n, l), 0.0);
-		float cos_fi = max(dot(n, h), 0.0);
+		if (!isShadowed) {
+			omega = shadowRay.dir;
+			h = normalize(omega + v);
+			cos_theta = max(dot(n, omega), 0.0);
+			cos_fi = max(dot(n, h), 0.0);
 
-		color += lights[i].intensity * (cos_theta * mtl.k_d + mtl.k_s * pow(cos_fi, mtl.n));	
+			vec3 diffuse  = cos_theta * mtl.k_d;
+			vec3 specular = mtl.k_s * pow(cos_fi, mtl.n);
+
+			color += lights[i].intensity * (diffuse + specular);
+		}
 	}
 	return color;
 }
@@ -62,7 +85,7 @@ vec3 Shade( Material mtl, vec3 position, vec3 normal, vec3 view )
 // Returns true if an intersection is found.
 bool IntersectRay( inout HitInfo hit, Ray ray )
 {
-	hit.t = 1e30;
+	hit.t = 1e30; 
 	bool foundHit = false;
 
 	// ray's parameters
@@ -74,29 +97,27 @@ bool IntersectRay( inout HitInfo hit, Ray ray )
 		// I have to solve a quadratic polynomial equation as function of t
 		// sphere's parameters
 		vec3 center = spheres[i].center;
-		float radius = spheres[i].radius;
+		float r = spheres[i].radius;
 
 		// coefficients of the quadratic function
 		float a = dot(d, d);
-		vec3 temp = p-c;
-		float b = 2 * dot(d, temp);
+		vec3 temp = p-center;
+		float b = 2.0 * dot(d, temp);
 		float c = dot(temp, temp) - r*r;
 
-		float delta = b*b - (4 * a * c);
-		if (delta < 0) continue; // no intersection
+		float delta = b*b - (4.0 * a * c);
+		if (delta < 0.0) continue; // no intersection
 		// TO-DO: If intersection is found, update the given HitInfo
 
-		foundHit = true;
-		float sqrt_delta = sqrt(delta);
-		float two_times_a = 2 * a;
-		float t1 = (-b + sqrt_delta) / two_times_a;
-		float t2 = (-b - sqrt_delta) / two_times_a;
-
-		float t = t1<t2 ? t1:t2;
-
-		if (t < hit.t){
+        float sq = sqrt(delta);
+        float t1 = (-b - sq) / (2.0 * a);
+        float t2 = (-b + sq) / (2.0 * a);
+        float t  = (t1 > 0.0) ? t1 : ((t2 > 0.0) ? t2 : -1.0);
+    
+		if (t > 0.0 && t < hit.t){
+			foundHit = true;
 			hit.t = t;
-			hit.position = pos + dir*t;
+			hit.position = p + d*t;
 			hit.normal = normalize(hit.position - center);
 			hit.mtl = spheres[i].mtl;
 		}
@@ -123,10 +144,16 @@ vec4 RayTracer( Ray ray )
 			HitInfo h;	// reflection hit info
 			
 			// TO-DO: Initialize the reflection ray
+			r.dir = normalize(2.0 * dot(view, hit.normal) * hit.normal - view);
+			r.pos = hit.position + normalize(hit.normal) * 0.001;
 			
 			if ( IntersectRay( h, r ) ) {
 				// TO-DO: Hit found, so shade the hit point
+				view = normalize( -r.dir);
+				clr += k_s * Shade( h.mtl, h.position, h.normal, view );
 				// TO-DO: Update the loop variables for tracing the next reflection ray
+				hit = h;
+				k_s *= hit.mtl.k_s;
 			} else {
 				// The refleciton ray did not intersect with anything,
 				// so we are using the environment color
