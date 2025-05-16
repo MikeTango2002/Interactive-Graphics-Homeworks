2b0208a3037ae0a9a3004bc0c0e8460c78e94894
MikeTango2002 <miketango2002@gmail.com>
2025-05-16 16:03:32 +0200

    Added the shading code from the previous homework

--- project_6/project7.js
+++ project_6/project7.js
@@ -5,13 +5,33 @@
 function GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
 {
 	// [TO-DO] Modify the code below to form the transformation matrix.
+	var rotationAxesX = [
+		1, 0, 0, 0,
+		0, Math.cos(rotationX), -Math.sin(rotationX), 0,
+		0, Math.sin(rotationX), Math.cos(rotationX), 0,
+		0, 0, 0, 1
+
+	];
+
+	var rotationAxesY = [
+		Math.cos(rotationY), 0, -Math.sin(rotationY), 0,
+		0, 1, 0, 0,
+		Math.sin(rotationY), 0, Math.cos(rotationY), 0,
+		0, 0, 0, 1
+
+	]
+
 	var trans = [
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		translationX, translationY, translationZ, 1
 	];
-	var mv = trans;
+
+	var rotations = MatrixMult(rotationAxesY, rotationAxesX);
+	
+	var mv = MatrixMult(trans, rotations);
+
 	return mv;
 }
 
@@ -24,6 +44,29 @@ class MeshDrawer
 	constructor()
 	{
 		// [TO-DO] initializations
+		// Compile the shader program
+		this.prog = InitShaderProgram( meshVS, meshFS );
+		
+		// Get the ids of the uniform variables in the shaders
+		this.mvp = gl.getUniformLocation( this.prog, 'mvp' ); // This variable stores the ModelViewProjection transformation matrix
+		this.mv = gl.getUniformLocation( this.prog, 'mv');   // This variable stores the ModelView transformation matrix
+		this.mnr = gl.getUniformLocation( this.prog, 'mnr'); // This variable stores the MatrixNormal transformation
+		
+		// Get the ids of the vertex attributes in the shaders
+		this.vertPos = gl.getAttribLocation( this.prog, 'pos' );
+		this.texCoords = gl.getAttribLocation(this.prog, 'txc');
+		this.normals = gl.getAttribLocation(this.prog, 'nrm');
+		
+		// Create the buffer objects
+		this.vertbuffer = gl.createBuffer();
+		this.texbuffer = gl.createBuffer();
+		this.normalsbuffer = gl.createBuffer();
+
+		this.u_swapYZ = gl.getUniformLocation(this.prog, 'u_swapYZ');
+
+
+		this.alpha = gl.getUniformLocation(this.prog, 'alpha'); // Exponent alpha of the Blinn material model
+		this.light_dir = gl.getUniformLocation(this.prog, 'light_dir'); // Light direction in camera space
 	}
 	
 	// This method is called every time the user opens an OBJ file.
@@ -41,6 +84,21 @@ class MeshDrawer
 	{
 		// [TO-DO] Update the contents of the vertex buffer objects.
 		this.numTriangles = vertPos.length / 3;
+		this.numVertices = vertPos.length;
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertbuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.DYNAMIC_DRAW);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.texbuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.DYNAMIC_DRAW);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.normalsbuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.DYNAMIC_DRAW);
+
+
+		// Unbind the gl.ARRAY_BUFFER buffer
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
 	}
 	
 	// This method is called when the user changes the state of the
@@ -49,6 +107,14 @@ class MeshDrawer
 	swapYZ( swap )
 	{
 		// [TO-DO] Set the uniform parameter(s) of the vertex shader
+
+		gl.useProgram(this.prog);
+		if (swap == true){
+			gl.uniform1i(this.u_swapYZ, true);
+		}
+		else{
+			gl.uniform1i(this.u_swapYZ, false);
+		}
 	}
 	
 	// This method is called to draw the triangular mesh.
@@ -59,6 +125,23 @@ class MeshDrawer
 	draw( matrixMVP, matrixMV, matrixNormal )
 	{
 		// [TO-DO] Complete the WebGL initializations before drawing
+		gl.useProgram(this.prog);
+		gl.uniformMatrix4fv( this.mvp, false, matrixMVP);
+		gl.uniformMatrix4fv( this.mv, false, matrixMV);
+		gl.uniformMatrix3fv( this.mnr, false, matrixNormal);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertbuffer);
+		gl.vertexAttribPointer(this.vertPos, 3, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.vertPos);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.texbuffer);
+		gl.vertexAttribPointer(this.texCoords, 2, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.texCoords);	
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.normalsbuffer);
+		gl.vertexAttribPointer(this.normals, 3, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.normals);
+
 
 		gl.drawArrays( gl.TRIANGLES, 0, this.numTriangles );
 	}
@@ -68,12 +151,54 @@ class MeshDrawer
 	setTexture( img )
 	{
 		// [TO-DO] Bind the texture
+		this.texture = gl.createTexture();
+
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
 
 		// You can set the texture image data using the following command.
 		gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img );
 
+		gl.generateMipmap(gl.TEXTURE_2D);
+
+		gl.texParameteri(
+			gl.TEXTURE_2D,
+			gl.TEXTURE_MAG_FILTER,
+			gl.LINEAR
+		);
+
+		gl.texParameteri(
+			gl.TEXTURE_2D,
+			gl.TEXTURE_MIN_FILTER,
+			gl.LINEAR_MIPMAP_LINEAR
+		);
+
+		gl.texParameteri(
+			gl.TEXTURE_2D,
+			gl.TEXTURE_WRAP_S,
+			gl.REPEAT
+		);
+
+		gl.texParameteri(
+			gl.TEXTURE_2D,
+			gl.TEXTURE_WRAP_T,
+			gl.REPEAT
+		);
+
+
 		// [TO-DO] Now that we have a texture, it might be a good idea to set
 		// some uniform parameter(s) of the fragment shader, so that it uses the texture.
+
+		gl.useProgram(this.prog);
+
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+		this.u_showTex = gl.getUniformLocation(this.prog, 'u_showTex');
+		this.sampler = gl.getUniformLocation(this.prog, 'tex');
+
+		gl.uniform1i(this.u_showTex, true);
+		gl.uniform1i(this.sampler, 0);
+
 	}
 	
 	// This method is called when the user changes the state of the
@@ -82,20 +207,139 @@ class MeshDrawer
 	showTexture( show )
 	{
 		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify if it should use the texture.
+		if(show == true){
+			gl.useProgram(this.prog);
+			gl.uniform1i(this.u_showTex, true);
+		}
+
+		else{
+			gl.useProgram(this.prog);
+			gl.uniform1i(this.u_showTex, false);
+		}
 	}
 	
 	// This method is called to set the incoming light direction
 	setLightDir( x, y, z )
 	{
 		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify the light direction.
+		let light_dir = new Float32Array([x, y, z]);
+
+	    // Normalize the light direction
+		let length = Math.sqrt(light_dir[0] * light_dir[0] + light_dir[1] * light_dir[1] + light_dir[2] * light_dir[2]);
+		light_dir[0] /= length;
+		light_dir[1] /= length;
+		light_dir[2] /= length;
+
+		light_dir[1] = -light_dir[1];
+
+	
+		gl.useProgram(this.prog);
+		gl.uniform3fv(this.light_dir, light_dir);
+
 	}
 	
 	// This method is called to set the shininess of the material
 	setShininess( shininess )
 	{
 		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify the shininess.
+		gl.useProgram(this.prog);
+		gl.uniform1f(this.alpha, shininess);
 	}
 }
+
+
+
+// Vertex shader source code
+var meshVS = `
+	attribute vec3 pos;
+	attribute vec2 txc;
+	attribute vec3 nrm;
+	uniform mat4 mvp;
+	uniform mat4 mv;
+	uniform mat3 mnr;
+	varying vec2 texCoord;
+
+	uniform bool u_swapYZ;
+
+	varying vec3 camPos;
+	varying vec3 camNorm;
+
+	void main()
+	{	
+		vec3 pos_copy = pos;
+		vec3 nrm_copy = nrm;
+
+		if(u_swapYZ){
+			float temp = pos_copy.y;
+			pos_copy.y = pos_copy.z;
+			pos_copy.z = temp;
+
+			temp = nrm_copy.y;
+			nrm_copy.y = nrm_copy.z;
+			nrm_copy.z = temp;
+		}
+
+		camPos = (mv * vec4(pos_copy, 1)).xyz;
+		camNorm = normalize(mnr * nrm_copy);
+
+		gl_Position = mvp * vec4(pos_copy,1);
+		texCoord = txc;
+
+	}
+`;
+// Fragment shader source code
+var meshFS = `
+	precision mediump float;
+	uniform sampler2D tex;
+	varying vec2 texCoord;
+
+	varying vec3 camPos;
+	varying vec3 camNorm;
+
+	uniform bool u_showTex;
+
+	uniform float alpha;
+	uniform vec3 light_dir;
+
+	void main()
+	{	
+
+		vec3 viewDir = normalize(-camPos);
+
+		vec3 intensity = vec3(1.0, 1.0, 1.0);
+
+		vec3 Ks = vec3(1.0, 1.0, 1.0);
+
+		vec3 Kd = vec3(1.0, 1.0, 1.0);
+
+		float opacity = 1.0;
+
+
+		if(u_showTex){	
+
+			vec3 texColor = texture2D(tex, texCoord).rgb;
+			Kd = texColor;
+			opacity = texture2D(tex, texCoord).a;
+		}
+
+		//float cos_theta = (camNorm.x * light_dir.x) + (camNorm.y * light_dir.y) + (camNorm.z * light_dir.z);
+		float cos_theta = max(dot(camNorm, light_dir), 0.0);
+
+		vec3 h = normalize(light_dir + viewDir);
+
+		//float cos_fi = (camNorm.x * h.x) + (camNorm.y * h.y) + (camNorm.z * h.z);
+		float cos_fi = max(dot(camNorm, h), 0.0);
+
+		vec3 diffuse = cos_theta * Kd;
+		vec3 specular = Ks * pow(cos_fi, alpha);
+
+		vec3 blinn_reflection_model = 0.1*Kd + intensity * (diffuse + specular);
+
+		gl_FragColor = vec4(blinn_reflection_model, opacity);
+		
+	}
+`;
+
 
 
 // This function is called for every step of the simulation.
